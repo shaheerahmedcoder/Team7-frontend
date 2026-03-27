3bae80da45ac01adcaa60b8d145ae963177a73a3
Shaheer Ahmed <shaheerahmed.0206@gmail.com>
2026-03-28 00:46:30 +0500

    code updated + purpose added for better understanding

--- constants/theme.ts
+++ constants/theme.ts
@@ -1,7 +1,7 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
+// Purpose of the file:
+// Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+
+
 
 import { Platform } from 'react-native';
 
