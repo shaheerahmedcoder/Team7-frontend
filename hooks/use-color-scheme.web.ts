37d46629365e2cc32e323978c772d1ad74fc7d52
Shaheer Ahmed <shaheerahmed.0206@gmail.com>
2026-03-27 23:55:19 +0500

    purpose added for understanding

--- hooks/use-color-scheme.web.ts
+++ hooks/use-color-scheme.web.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useColorScheme as useRNColorScheme } from 'react-native';
 
+//Purpose of the file
 /**
  * To support static rendering, this value needs to be re-calculated on the client side for web
  */
